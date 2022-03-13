6cc36b4c9c1d4e75512495c558aed133d4f0ba3a
Aleksey Barabanov <forarbaiten@yandex.ru>
2022-03-13 17:05:13 +0300

    feature/addImage Added addimage form

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,36 @@
 const popup=document.querySelector('.popup');
 const profileOpenButton=document.querySelector('.profile__edit-button');
+const profileAddButton=document.querySelector('.profile__add-button');
 const profileCloseButton=popup.querySelector('.popup__close');
 
 let profileName=document.querySelector('.profile__name');
 let profileDescript=document.querySelector('.profile__description');
-let popupName=popup.querySelector('.popup__input_type_name');
-let popupDescript=popup.querySelector('.popup__input_type_description');
-let popupSaveButton=popup.querySelector('.popup__inputs');
+const popupName=popup.querySelector('.popup__input_type_name');
+const popupDescript=popup.querySelector('.popup__input_type_description');
+const popupTitle=popup.querySelector('.popup__input_type_title');
+const popupLink=popup.querySelector('.popup__input_type_link');
+const popupSaveButton=popup.querySelector('.popup__inputs');
 
-function togglePopup(){
+function openPopupEdit(){
     let userName=profileName.textContent;
     let userProfile=profileDescript.textContent;
     popupName.value=userName;
     popupDescript.value=userProfile;
-    popup.classList.toggle('popup_active');
+    popupName.removeAttribute('hidden');
+    popupDescript.removeAttribute('hidden');
+    popup.classList.add('popup_active');
+}
+function openPopupAdd(){
+    popupTitle.removeAttribute('hidden');
+    popupLink.removeAttribute('hidden');
+    popup.classList.add('popup_active');
 }
 function closePopup(){
     popup.classList.remove('popup_active');
+    popupTitle.setAttribute('hidden','hidden');
+    popupLink.setAttribute('hidden','hidden');
+    popupName.setAttribute('hidden','hidden');
+    popupDescript.setAttribute('hidden','hidden');
 }
 function saveProfile(evt){
     evt.preventDefault();
@@ -25,7 +39,8 @@ function saveProfile(evt){
     closePopup();
 }
 
-profileOpenButton.addEventListener('click',togglePopup);
+profileOpenButton.addEventListener('click',openPopupEdit);
+profileAddButton.addEventListener('click',openPopupAdd);
 profileCloseButton.addEventListener('click',closePopup);
 popupSaveButton.addEventListener('submit',saveProfile);
 popup.addEventListener('click',function(event){
